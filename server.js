9ad6608553c84c77821864b3df5b5c0e3f06f28a
kumarsushant36 <kumar.sushant36@gmail.com>
2016-10-06 02:12:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,12 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var counter=0;
+app.get('/counter',function(req, res){
+   counter=counter+1;
+   req.send(counter.tostring());
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
