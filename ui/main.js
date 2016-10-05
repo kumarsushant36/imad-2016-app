b98987df8972fb7a8227f136eee02d943b8ef9c4
kumarsushant36 <kumar.sushant36@gmail.com>
2016-10-06 03:02:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,19 @@
-console.log('Loaded!');
-//Move the image
 
-var img=document.getElementById("madi");
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft + "px";
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,100);
-};
 //counter
-var counter=0;
+
 var button=document.getElementById("counter");
+
 button.onclick= function(){
-    counter=counter+1;
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200){
+    var counter=request.ResponseText;
     var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
+    span.innerHTML=counter.toString();  
+        }
+        }
+    };
+    request.open('GET','http://kumarsushant36.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
