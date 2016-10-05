4d97c2fa6fc008984e586502054606d7b2f6533c
kumarsushant36 <kumar.sushant36@gmail.com>
2016-10-06 03:55:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,10 @@ var name=nameInput.value;
 var submit=document.getElementById("Submit_btn");
 submit.onclick=function(){
     
-    
+     var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200){
     var names=['name1','name2','name3'];
     var list='';
     for(var i=0;i<names.length;i++){
@@ -30,5 +33,10 @@ submit.onclick=function(){
     }
     var ul=document.getElementById("namelist");
     ul.innerHTML=list;
-    
+        }
+    }
+   
+    };
+    request.open('GET','http://kumarsushant36.imad.hasura-app.io/counter',true);
+    request.send(null); 
 };
